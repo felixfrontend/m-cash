191512e5f585edf488cce3bb61987e3bc646e28c
felixfrontend <69413467+felixfrontend@users.noreply.github.com>
2020-08-19 01:03:48 +0600

    our code

---
+++ m-cash/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+	$('.review__slider').slick({
+		arrows:false,
+		variableWidth:true,
+		variableHeight:true,
+		autoplay:true,
+		autoplaySpeed:4000,
+		dots:false,
+		adaptiveHeight:false,
+		waitForAnimate:false,
+		centerMode:true,
+		// appendArrows:$('.btn'),
+	});
+})
